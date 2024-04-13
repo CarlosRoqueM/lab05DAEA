95670a2a367f6efe69ed89e2c6ea88cb0a82a3bc
Alumno <Alumno@LAB1504-04>
2024-04-13 12:56:51 -0500

    Agregar archivos de proyecto.

---
+++ lab05DAEA/Lisar.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lab05DAEA
+{
+    /// <summary>
+    /// Lógica de interacción para Lisar.xaml
+    /// </summary>
+    public partial class Lisar : Window
+    {
+        public Lisar()
+        {
+            InitializeComponent();
+            CargaDatos();
+        }
+
+        private void CargaDatos()
+        {
+            string ConnectionString = "Data Source= LAB1504-04\\SQLEXPRESS; Initial Catalog=lab05;" + "User Id = Carlos; Password=123456";
+
+            List<Cliente> clientes = new List<Cliente>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(ConnectionString);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("lisarClientes2", connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string idCliente = reader.GetString("idCliente");
+                    string NombreCompañia = reader.GetString("NombreCompañia");
+                    string? NombreContacto = reader.GetString("NombreCompañia");
+                    string? Direccion = reader.GetString("Direccion");
+                    //string Region = reader.GetString("Region");
+                    string? CodPostal = reader.GetString("CodPostal");
+                    string? Pais = reader.GetString("Pais");
+                    string? Telefono = reader.GetString("Telefono");
+                    //string Fax = reader.GetString("Fax");
+
+
+                    clientes.Add(new Cliente { idCliente = idCliente, NombreCompañia = NombreCompañia, NombreContacto = NombreContacto, Direccion = Direccion, CodPostal = CodPostal, Pais = Pais, Telefono  = Telefono });
+
+                  
+                }
+                connection.Close();
+
+                Number01.ItemsSource = clientes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab05DAEA/Opciones.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab05DAEA
+{
+    /// <summary>
+    /// Lógica de interacción para Opciones.xaml
+    /// </summary>
+    public partial class Opciones : Window
+    {
+        public Opciones()
+        {
+            InitializeComponent();
+        }
+
+        private void ListarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Lisar listar = new Lisar();
+            listar.Show();
+            Close();
+        }
+
+        private void EliminarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Eliminar eliminar = new Eliminar();
+            eliminar.Show();
+            Close();
+        }
+
+        private void InsertarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Crear crear = new Crear();  
+            crear.Show();   
+            Close();
+        }
+    }
+}
